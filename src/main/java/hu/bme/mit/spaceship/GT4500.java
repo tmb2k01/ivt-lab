c51ec585b0759a5d99cdc26b90d247ee4bb8e179
tmb2k01 <marcsello.ma@gmail.com>
2023-04-21 09:24:49 +0200

    repair code

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,9 +77,8 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
-        //TODO implement feature
-
+        if(! primaryTorpedoStore.isEmpty() &&  !secondaryTorpedoStore.isEmpty())
+          firingSuccess = primaryTorpedoStore.fire(1) &&  secondaryTorpedoStore.fire(1);
         break;
     }
 
